b1deb477707c577063e180836f15ce4573edea9e
James Clarke <jkc200013@gmail.com>
2019-08-29 20:12:43 +0100

    added native manager/env templates

---
+++ wavynet/vm/native/native_env.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * James Clarke
+ * 22/08/19
+ */
+
+namespace wavynet.vm.native
+{
+    /*
+     * This class is responsible for providing 
+     * an interface to the VM from native code
+     */
+    class NativeEnv
+    {
+    }
+}
---
+++ wavynet/vm/native/native_interface.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * James Clarke
+ * 29/08/19
+ */
+
+namespace wavynet.vm.native
+{
+    /*
+     * Class used to handle & dispatch native method calls from the vm
+     */
+    class NativeInterface
+    {
+
+    }
+}
